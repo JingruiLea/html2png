2b9228f751ab54c352f0a4a19fb95a0ef1d50b78
JingruiLea <jingrui.lea@gmail.com>
2023-11-21 14:00:49 +0800

    new token

--- app.js
+++ app.js
@@ -45,6 +45,38 @@ app.post('/html2png', async (req, res) => {
   res.send(screenshot)
 })
 
+app.post('/html2pdf', async (req, res) => {
+  console.log('Received request for PDF conversion')
+
+  const url = req.body.url
+  const format = req.body.format || 'A4'
+
+  if (!url) {
+    return res.status(400).send({ error: 'Missing URL' })
+  }
+
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  try {
+    await page.goto(url, { waitUntil: 'networkidle2' })
+  } catch (error) {
+    await browser.close()
+    return res.status(400).send({ error: 'Invalid URL' })
+  }
+
+  // 将网页内容渲染为PDF
+  const pdf = await page.pdf({
+    format: format,
+    printBackground: true,
+  })
+
+  await browser.close()
+
+  res.setHeader('Content-Type', 'application/pdf')
+  res.send(pdf)
+})
+
 const port = process.env.PORT || 3000
 const ip = '0.0.0.0'
 app.listen(port, ip, () => console.log(`Server is running on ${ip}:${port}`))
